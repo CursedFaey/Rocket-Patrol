644e9ef519ab48e74c127fa3f06e5b427688b13d
Ian larson <94594696+CursedFaey@users.noreply.github.com>
2022-04-19 13:59:11 -0700

    fix #1

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -1,16 +1,14 @@
 class Spaceship extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame, pointValue) {
       super(scene, x, y, texture, frame);
-  
-      // add object to existing scene
-      scene.add.existing(this);   // add to existing, displayList, updateList
-      this.points = pointValue;      //track rocket's firing status
-      this.moveSpeed = 3;         //pixels per frame
+      scene.add.existing(this);
+      this.points = pointValue;
+      this.moveSpeed = 3; 
     }
 
     update() {
         this.x -= this.moveSpeed;
-        if(this.x <= 0 - this.width){
+        if(this.x <= 0 - this.width) {
             this. x = game.config.width;
         }
     }
